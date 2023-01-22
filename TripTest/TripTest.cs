e1c407fd075fe55607bdd70c0ff6e87554714dc5
bnkala <nkala.bryan3@gmail.com>
2023-01-22 02:02:54 +0200

    Removed unused lines

--- TripTest/TripTest.cs
+++ TripTest/TripTest.cs
@@ -9,8 +9,6 @@ namespace TripTestNS
         public void TestFare()
         {
             Trip t1 = new Trip("111 RAJ", "Greenfields", 15.0);
-            //String result = t1.toString();
-            //Assert.AreEqual("Car Id: 111 RAJ\nDestination: Greenfields\nDistance: 15km\nTotal Fare payable: R50", result);
             double result = t1.Total_Fare();
             Assert.AreEqual(50.0, result);
 
